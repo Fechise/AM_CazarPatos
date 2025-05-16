320560914bfdc098282fbabd1e6b5f74b4d8d8f6
Andrés Mateo Quillupangui Osorio <andres.quillupangui@epn.edu.ec>
2025-05-16 15:17:49 -0500

    Added Log In Activity

--- app/src/main/java/com/quillupangui/andres/cazarpatos/LogInActivity.kt
+++ app/src/main/java/com/quillupangui/andres/cazarpatos/LogInActivity.kt
@@ -1,20 +1,30 @@
 package com.quillupangui.andres.cazarpatos
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
 
-class MainActivity2 : AppCompatActivity() {
+class LogInActivity : AppCompatActivity() {
+
+    private lateinit var buttonLogIn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_login)
+        buttonLogIn = findViewById(R.id.button_LogIn)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        buttonLogIn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
         }
     }
 }
